b0bf34a373f05b804e6e558505280af96d327e0e
José Ariel Liriano <52571899+ArielLiriano@users.noreply.github.com>
2024-07-29 18:30:53 -0400

    Juego de pokemons en un 35%

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,24 @@
+<template>
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animate-pulse">Cargando pokémons</h3>
+  </section>
+
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Quién es este Pokémon?</h1>
+
+    <!--==============Pokémon picture===============-->
+    <PokemonPicture />
+
+    <!--==============Pokémon options===============-->
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+
+const { gameStatus, isLoading } = usePokemonGame();
+</script>
